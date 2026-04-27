7dc9196fea42e4895648d76362035ef420b8689d
SimonP <138118450+SPalu2097@users.noreply.github.com>
2026-04-27 12:57:28 +0300

    Add session management to admin.php
    add session

--- admin.php
+++ admin.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header('Location: login.php');
+    exit();
+}
+?>
 <h1>admin</h1>
 <a href="lisa.php">+ Lisa auto</a>
 <table border="1">
