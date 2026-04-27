ea9f99500f087b9b3450256c7ba52c072b0e54e8
SimonP <138118450+SPalu2097@users.noreply.github.com>
2026-04-27 12:59:56 +0300

    Add session check before deleting record

--- kustuta.php
+++ kustuta.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header('Location: login.php');
+    exit();
+}
     include("config.php");
         $paring = "DELETE FROM cr_simon WHERE id = ".$_GET['id']."";
         $valjund = mysqli_query($yhendus, $paring);
